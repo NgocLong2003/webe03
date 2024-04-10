217d228380151074ba54608e03efa1f5afcc7958
NgocLong2003 <long2003.forwork@gmail.com>
2024-04-11 02:21:20 +0700

    Add all file e03

---
+++ conn.php
@@ -0,0 +1,16 @@
+<?php 
+
+$host = "127.0.0.1";
+$user = "root";
+$pass = "123";
+$db   = "e03";
+$conn = null;
+
+try {
+  $conn = new PDO("mysql:host={$host};dbname={$db};",$user,$pass);
+} catch (Exception $e) {
+      
+}
+
+
+ ?>
